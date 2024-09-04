6fbb3d724cc02196dfa2e8a8865e48bba6a8cec7
maruthikumars <maruthi.wsi@gmail.com>
2024-09-04 13:58:08 +0530

    os

---
+++ JULY/EX_05072024/LAB176.py
@@ -0,0 +1,3 @@
+import os
+all_dir = os.listdir("/VENKATESH/PYTHON ATB3X/JULY/EX_05072024")
+print(all_dir)
---
+++ JULY/EX_05072024/LAB177.py
@@ -0,0 +1,6 @@
+import os
+
+for root, dir, files in os.walk("/VENKATESH/PYTHON ATB3X/JULY/EX_05072024"):
+    print(f"Current Dir {root}")
+    print(f"Sub Dir Dir {dir}")
+    print(f"files Dir Dir {files}")
